a95ddec150dc12adcb74c56b923ce15a6f5c444b
Farhad <farkhadcity@gmail.com>
2016-06-07 06:27:41 +0300

    add topics

--- mForum/client/app.js
+++ mForum/client/app.js
@@ -1,1 +1,18 @@
-angular.module('forum', ['angular-meteor']);
+angular.module('forum', ['angular-meteor', 'ui.router'])
+.config(function($urlRouterProvider, $stateProvider){
+
+    //set the default route
+    $urlRouterProvider
+        .when('/', '/topics')
+        .otherwise('/topics');
+
+    //Add states
+    $stateProvider.state('topics', {
+        url: '/topics',
+        templateUrl: 'views/pages/topics.html'
+    });
+
+})
+    .run(function($state){
+        // we inject $state here to initialize ui.router
+    })
