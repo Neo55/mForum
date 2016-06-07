cb27c56c0f30d57203f08b8017a7dc3a87ea549e
Farhad <farkhadcity@gmail.com>
2016-06-07 06:55:04 +0300

    add new packages for accounts

--- mForum/client/app.js
+++ mForum/client/app.js
@@ -14,6 +14,12 @@ angular.module('forum', ['angular-meteor', 'ui.router'])
         controller: 'TopicsController'
     });
 
+        $stateProvider.state('topic', {
+            url: '/topic/:topicId',
+            templateUrl: 'views/pages/topic.html',
+            controller: 'TopicController'
+        });
+
 })
     .run(function($state){
         // we inject $state here to initialize ui.router
@@ -24,6 +30,15 @@ angular.module('forum', ['angular-meteor', 'ui.router'])
         $scope.helpers({
             topics: function(){
                 return Topics.find({}, {sort: {name:1}});
+            }
+        });
+    })
+
+.controller('TopiController', function($scope, $stateParams){
+        $scope.subscribe('topic', function(){ return [$stateParams.topicId]; });
+        $scope.helpers({
+            topic: function(){
+                return Topics.findOne({_id: $stateParams.topicId});
             }
         });
     })
