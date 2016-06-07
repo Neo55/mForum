d2749d4d9ac50a9a94221593cf58ce04dab8d97a
Farhad <farkhadcity@gmail.com>
2016-06-07 06:44:24 +0300

    add controller

--- mForum/client/app.js
+++ mForum/client/app.js
@@ -1,4 +1,5 @@
 angular.module('forum', ['angular-meteor', 'ui.router'])
+
 .config(function($urlRouterProvider, $stateProvider){
 
     //set the default route
@@ -9,10 +10,20 @@ angular.module('forum', ['angular-meteor', 'ui.router'])
     //Add states
     $stateProvider.state('topics', {
         url: '/topics',
-        templateUrl: 'views/pages/topics.html'
+        templateUrl: 'views/pages/topics.html',
+        controller: 'TopicsController'
     });
 
 })
     .run(function($state){
         // we inject $state here to initialize ui.router
+    })
+
+.controller('TopicsController', function($scope){
+        $scope.subscribe('topics');
+        $scope.helpers({
+            topics: function(){
+                return Topics.find({}, {sort: {name:1}});
+            }
+        });
     })
